3ce8e70ea2552a2d25660cc74ce931cfff03e831
Daimoonrussell <daimoonrussell@gmail.com>
2025-01-19 22:06:03 +0100

    create function called getHumanChoice

--- javascript.js
+++ javascript.js
@@ -1,16 +1,22 @@
-//pseudokod
 // create function called getComputerChoice
-function getComputerChoice() {
-    const randomNumber = Math.random();
-    if (randomNumber < 0.33){
-        return "scissors"
-    } else if(randomNumber < 0.66) {
+function getComputerChoice(){
+    randomNumber = Math.random();
+    if (randomNumber<0.33){
+        return "rock"
+    } else if (randomNumber <0.66){
         return "paper"
     } else {
-        return "rock"
+        return "scissors"
     }
 }
 
-
 console.log(getComputerChoice())
-//
+
+// create function called getHumanChoice
+function getHumanChoice(){
+    const userChoice = prompt("paper,scissors or rock?\nmake a choice.")
+        return userChoice
+}
+
+
+console.log(getHumanChoice())
